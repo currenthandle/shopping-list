601a95c114fd7ebfd8b3474654e29f067edc624b
Casey <leptone55@gmail.com>
2022-11-11 10:51:01 -0800

    auth working for login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,6 @@
 import NextAuth, { type NextAuthOptions } from 'next-auth'
-//import DiscordProvider from "next-auth/providers/discord";
-// Prisma adapter for NextAuth, optional and can be removed
 import CredentialsProvider from 'next-auth/providers/credentials'
 import bcrypt from 'bcrypt'
-
-//import { PrismaAdapter } from '@next-auth/prisma-adapter'
 
 import { env } from '../../../env/server.mjs'
 import { prisma } from '../../../server/db/client'
@@ -14,17 +10,6 @@ export const authOptions: NextAuthOptions = {
     //signIn: '/login',
     newUser: '/signUp',
   },
-  // Include user.id on session
-  // callbacks: {
-  //   session({ session, user }) {
-  //     if (session.user) {
-  //       session.user.id = user.id
-  //     }
-  //     return session
-  //   },
-  // },
-  // Configure one or more authentication providers
-  //adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
       name: 'Credentials',
@@ -61,6 +46,7 @@ export const authOptions: NextAuthOptions = {
         } catch (error) {
           console.error(error)
         }
+        return null
       },
     }),
   ],
